33e5f605d9b0f90fc48bf3772f1b54e3b0f42eb5
icomansari <ansari@icomdatasystems.com>
2022-08-03 14:24:39 +0530

    Fix for sale report to add agent code

--- uimgr/bpsalesrprtexcel.php
+++ uimgr/bpsalesrprtexcel.php
@@ -61,32 +61,32 @@ $objPHPExcel = new PHPExcel();
 		}
 			if($creteria == "BT") {
 			if($type == "ALL") {
-				$query .= " and date(date_time) >= '$startDate' and  date(date_time) <= '$endDate' group by a.bp_service_order_no,b.agent_name,b.parent_code,c.account_no,c.account_name,d.feature_description,b.agent_code order by date_time desc ";
+				$query .= " and date(date_time) >= '$startDate' and  date(date_time) <= '$endDate' group by a.bp_service_order_no,b.agent_name,b.parent_code,c.account_no,c.account_name,d.feature_description,b.agent_code,N.bp_opay_service_provider_name,c.update_time  order by date_time desc ";
 			}
 			else{
-					$query .= " and a.service_feature_code = '$type' and date(date_time) >= '$startDate' and  date(date_time) <= '$endDate' group by a.bp_service_order_no,b.agent_name,b.parent_code,c.account_no,c.account_name,d.feature_description ,b.agent_code order by date_time desc ";
+					$query .= " and a.service_feature_code = '$type' and date(date_time) >= '$startDate' and  date(date_time) <= '$endDate' group by a.bp_service_order_no,b.agent_name,b.parent_code,c.account_no,c.account_name,d.feature_description ,b.agent_code,N.bp_opay_service_provider_name,c.update_time  order by date_time desc ";
 			}
 		}
 		if($creteria == "BO") {
-			$query .= " and a.bp_service_order_no = $orderNo group by a.bp_service_order_no,b.agent_name,b.parent_code,c.account_no,d.feature_description,c.account_name,b.agent_code order by a.bp_service_order_no";
+			$query .= " and a.bp_service_order_no = $orderNo group by a.bp_service_order_no,b.agent_name,b.parent_code,c.account_no,d.feature_description,c.account_name,b.agent_code,N.bp_opay_service_provider_name,c.update_time  order by a.bp_service_order_no";
 		}
 		if($creteria == "C") { 
 			if($championCode == "ALL") {
-				$query .= " and date(date_time) >= '$startDate' and  date(date_time) <= '$endDate' group by a.bp_service_order_no,b.agent_name,b.parent_code,c.account_no,c.account_name,d.feature_description,b.agent_code,c.update_time order by date_time desc ";
+				$query .= " and date(date_time) >= '$startDate' and  date(date_time) <= '$endDate' group by a.bp_service_order_no,b.agent_name,b.parent_code,c.account_no,c.account_name,d.feature_description,b.agent_code,N.bp_opay_service_provider_name,c.update_time  order by date_time desc ";
 			}
 			else{ 
-				$query .= " and b.parent_code = '$championCode' and date(date_time) >= '$startDate' and  date(date_time) <= '$endDate' group by a.bp_service_order_no,b.agent_name,c.update_time,c.account_no,c.account_name,b.agent_code,c.update_time order by date_time desc ";
+				$query .= " and b.parent_code = '$championCode' and date(date_time) >= '$startDate' and  date(date_time) <= '$endDate' group by a.bp_service_order_no,b.agent_name,c.update_time,c.account_no,c.account_name,b.agent_code,N.bp_opay_service_provider_name,c.update_time  order by date_time desc ";
 			}
 		}
 		if($creteria == "S") { 
 			if($state == "ALL") {
-				$query .= " and date(date_time) >= '$startDate' and  date(date_time) <= '$endDate' group by a.bp_service_order_no,b.agent_name,c.update_time,c.account_no,c.account_name,b.agent_code,c.update_time,b.parent_code order by date_time desc ";
+				$query .= " and date(date_time) >= '$startDate' and  date(date_time) <= '$endDate' group by a.bp_service_order_no,b.agent_name,c.update_time,c.account_no,c.account_name,b.agent_code,N.bp_opay_service_provider_name,c.update_time ,b.parent_code order by date_time desc ";
 			}
 			else{ 
 			if($local_govt_id == ""){
-				$query .= " and b.state_id = '$state'   and date(date_time) >= '$startDate' and  date(date_time) <= '$endDate' group by a.bp_service_order_no,b.agent_name,c.update_time,c.account_no,c.account_name,b.agent_code,c.update_time,b.parent_code order by date_time desc ";
+				$query .= " and b.state_id = '$state'   and date(date_time) >= '$startDate' and  date(date_time) <= '$endDate' group by a.bp_service_order_no,b.agent_name,c.update_time,c.account_no,c.account_name,b.agent_code,N.bp_opay_service_provider_name,c.update_time ,b.parent_code order by date_time desc ";
 			}else{
-				$query .= " and b.state_id = '$state' and b.local_govt_id='$local_govt_id'  and date(date_time) >= '$startDate' and  date(date_time) <= '$endDate' group by a.bp_service_order_no,b.agent_name,c.update_time,c.account_no,c.account_name,b.agent_code,c.update_time,b.parent_code order by date_time desc ";
+				$query .= " and b.state_id = '$state' and b.local_govt_id='$local_govt_id'  and date(date_time) >= '$startDate' and  date(date_time) <= '$endDate' group by a.bp_service_order_no,b.agent_name,c.update_time,c.account_no,c.account_name,b.agent_code,N.bp_opay_service_provider_name,c.update_time ,b.parent_code order by date_time desc ";
 			}
 				
 			}
