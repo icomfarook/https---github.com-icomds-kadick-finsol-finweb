1f2f808ec932124bf33fbf80edf4906b36b4c2e7
icomansari <ansari@icomdatasystems.com>
2022-08-22 21:16:30 +0530

    fix for monthly sanef report

--- uirot/monthreportexcel.php
+++ uirot/monthreportexcel.php
@@ -1,72 +1,64 @@
 <?php
-ERROR_REPORTING(E_ALL);
-$data = json_decode(file_get_contents("php://input"));
-require('../common/admin/configmysql.php');
-require('../common/sessioncheck.php');
-//include("excelfunctions.php");
-require_once   '../common/PHPExcel/Classes/PHPExcel/IOFactory.php';
-   $agentCode	= $_POST['agentCode'];
-   $MonthDate	= $_POST['MonthDate'];	
-   $MonthDate = date("Y-m", strtotime($MonthDate,'-1 month'));
-   //$MonthDate = date("Y-m", strtotime($MonthDate));
-       
-$title = "KadickMoni";
+  ERROR_REPORTING(E_ALL);
+  $data = json_decode(file_get_contents("php://input"));
+  require('../common/admin/configmysql.php');
+  require('../common/sessioncheck.php');
+  //include("excelfunctions.php");
+  require_once   '../common/PHPExcel/Classes/PHPExcel/IOFactory.php';
+  $agentCode	= $_POST['agentCode'];
+  $MonthDate	= $_POST['MonthDate'];	
+  $MonthDate = date("Y-m", strtotime($MonthDate,'-1 month'));
+  //$MonthDate = date("Y-m", strtotime($MonthDate));
+  $title = "KadickMoni";
 
-$msg = "Monthly Report between $MonthDate ";
-$objPHPExcel = new PHPExcel();
-
-           
-                $heading = array("Type","Month");
-                $headcount = 3;
-                $query1 = "select 'Cashin' as type, format(ifNULL(sum(cashin_count),0),0) as count, format(ifNULL(sum(cashin_amount),0),2) as value from party_wallet_history where year(run_date) = year(current_date()) and month(run_date) = month(current_date()) and party_sales_type = 'External Agent' and date_format(run_date,'%Y-%m') = '$MonthDate' UNION ALL select 'Cashout' as type, format(ifNULL(sum(cashout_count),0),0) as count, format(ifNULL(sum(cashout_amount),0),2) as value from party_wallet_history where year(run_date) = year(current_date()) and month(run_date) = month(current_date()) and party_sales_type = 'External Agent' and date_format(run_date,'%Y-%m') = '$MonthDate'  UNION ALL  select 'Billpayemnt' as type, format(ifNULL(sum(billpay_count),0),0) as count, format(ifNULL(sum(billpay_amount),0),2) as value from party_wallet_history where year(run_date) = year(current_date()) and month(run_date) = month(current_date()) and party_sales_type = 'External Agent' and date_format(run_date,'%Y-%m') = '$MonthDate'  UNION ALL  select 'Airtime Recharge' as type, format(ifNULL(sum(evd_count),0),0) as count, format(ifNULL(sum(evd_amount),0),2) as value from party_wallet_history where year(run_date) = year(current_date()) and month(run_date) = month(current_date()) and party_sales_type = 'External Agent'  and date_format(run_date,'%Y-%m') = '$MonthDate' UNION ALL select 'Total' as type, format(sum(ifnull(cashin_count,0))+ sum(ifnull(cashout_count,0))+sum(ifnull(evd_count,0)) + sum(ifnull(billpay_count,0)),0) as count, format(sum(ifnull(cashin_amount,0)) + sum(ifnull(cashout_amount,0)) + sum(ifnull(billpay_amount,0)) + sum(ifnull(evd_count,0)),2) as value from party_wallet_history where year(run_date) = year(current_date()) and month(run_date) = month(current_date()) and date_format(run_date,'%Y-%m') = '$MonthDate'";
-
-
-        
-                   error_log("EXcelQuery".$query1);
-       $result =  mysqli_query($con,$query1);
-       if (!$result) {
-           printf("Error: %s\n".mysqli_error($con));
-           //exit();
-       }
-       
-
-	function generateExcel ($i,$row,$objPHPExcel,$column) { 
-        $Header=array("A", "B", "C", "D", "E","F","G", "H", "I", "J", "K", "L", "M","N","O","P","Q","R","S","T","U");       
-         /*   $Header2=array("A16", "B16", "C16", "D16", "E16","F16","G16", "H16", "I16", "J16", "K16", "L16", "M16","N16","O16","P16","Q16","R16","S16","T16","U16");   */       
-        $style = array(
+  $msg = "Monthly Report between $MonthDate ";
+  $objPHPExcel = new PHPExcel();
+  $heading = array("Type","Month");
+  $headcount = 3;
+  $query1 = "select 'Cashin' as type, format(ifNULL(sum(cashin_count),0),0) as count, format(ifNULL(sum(cashin_amount),0),2) as value from party_wallet_history where year(run_date) = year(current_date()) and month(run_date) = month(current_date()) and party_sales_type = 'External Agent' and date_format(run_date,'%Y-%m') = '$MonthDate' UNION ALL select 'Cashout' as type, format(ifNULL(sum(cashout_count),0),0) as count, format(ifNULL(sum(cashout_amount),0),2) as value from party_wallet_history where year(run_date) = year(current_date()) and month(run_date) = month(current_date()) and party_sales_type = 'External Agent' and date_format(run_date,'%Y-%m') = '$MonthDate'  UNION ALL  select 'Billpayemnt' as type, format(ifNULL(sum(billpay_count),0),0) as count, format(ifNULL(sum(billpay_amount),0),2) as value from party_wallet_history where year(run_date) = year(current_date()) and month(run_date) = month(current_date()) and party_sales_type = 'External Agent' and date_format(run_date,'%Y-%m') = '$MonthDate' UNION ALL select 'Airtime Recharge' as type, format(ifNULL(sum(evd_count),0),0) as count, format(ifNULL(sum(evd_amount),0),2) as value from party_wallet_history where year(run_date) = year(current_date()) and month(run_date) = month(current_date()) and party_sales_type = 'External Agent'  and date_format(run_date,'%Y-%m') = '$MonthDate' UNION ALL select 'Total' as type, format(sum(ifnull(cashin_count,0))+ sum(ifnull(cashout_count,0))+sum(ifnull(evd_count,0)) + sum(ifnull(billpay_count,0)),0) as count, format(sum(ifnull(cashin_amount,0)) + sum(ifnull(cashout_amount,0)) + sum(ifnull(billpay_amount,0)) + sum(ifnull(evd_count,0)),2) as value from party_wallet_history where year(run_date) = year(current_date()) and month(run_date) = month(current_date()) and date_format(run_date,'%Y-%m') = '$MonthDate'";
+  error_log("Excel Query1 = ".$query1);
+  $result =  mysqli_query($con,$query1);
+  if (!$result) {
+    printf("Error: %s\n".mysqli_error($con));
+    //exit();
+  }
+   
+  function generateExcel ($i,$row,$objPHPExcel,$column) { 
+    $Header=array("A", "B", "C", "D", "E","F","G", "H", "I", "J", "K", "L", "M","N","O","P","Q","R","S","T","U");       
+      /*   $Header2=array("A16", "B16", "C16", "D16", "E16","F16","G16", "H16", "I16", "J16", "K16", "L16", "M16","N16","O16","P16","Q16","R16","S16","T16","U16");   */       
+      $style = array(
         'alignment' => array(
             'horizontal' => PHPExcel_Style_Alignment::HORIZONTAL_CENTER,
-            )
-        );    
-		$styleArray = array(
-		  'borders' => array(
-			'allborders' => array(
-			  'style' => PHPExcel_Style_Border::BORDER_THIN
-			)
-		  )
-		); 		
+          )
+      );    
+		  $styleArray = array(
+		    'borders' => array(
+			    'allborders' => array(
+			      'style' => PHPExcel_Style_Border::BORDER_THIN
+			    )
+		    )
+		  ); 		
         
-        for($j=0; $j<$column; $j++) {        
-		  $objPHPExcel->getActiveSheet()->setCellValue("$Header[$j]"."$i","$row[$j]");
-          $objPHPExcel->getActiveSheet()->getColumnDimension($Header[$j])->setWidth(25);
-          $objPHPExcel->setActiveSheetIndex(0)->mergeCells('B1:C1');
-            $objPHPExcel->setActiveSheetIndex(0)->mergeCells('B16:C16:D13:E16:F16:G16');
-            $objPHPExcel->getActiveSheet()->setCellValue('A2', '');
-            $objPHPExcel->getActiveSheet()->setCellValue('A14', '');
-            $objPHPExcel->getActiveSheet()->setCellValue('B2', 'Count');
-            $objPHPExcel->setActiveSheetIndex(0)->mergeCells('B17:C17');
-            $objPHPExcel->setActiveSheetIndex(0)->mergeCells('D17:E17');
-            $objPHPExcel->setActiveSheetIndex(0)->mergeCells('F17:G17');
-            $objPHPExcel->getActiveSheet()->setCellValue('B16', 'Month');
-            $objPHPExcel->getActiveSheet()->setCellValue('B17', 'Air Time');
-            $objPHPExcel->getActiveSheet()->setCellValue('D17', 'Cash Sales');
-            $objPHPExcel->getActiveSheet()->setCellValue('F17', 'Bill Payment');
-            $objPHPExcel->getActiveSheet()->setCellValue('C2', 'Value');
-            $objPHPExcel->getActiveSheet()->setCellValue('A16', 'Region');
-
-		  $objPHPExcel->getActiveSheet()->getDefaultStyle()->applyFromArray($style);
-		  $objPHPExcel->getActiveSheet()->getStyle("$Header[$j]"."$i")->applyFromArray($styleArray);		  
-        }    
+      for($j=0; $j<$column; $j++) {        
+		    $objPHPExcel->getActiveSheet()->setCellValue("$Header[$j]"."$i","$row[$j]");
+        $objPHPExcel->getActiveSheet()->getColumnDimension($Header[$j])->setWidth(25);
+        $objPHPExcel->setActiveSheetIndex(0)->mergeCells('B1:C1');
+        $objPHPExcel->setActiveSheetIndex(0)->mergeCells('B16:C16:D13:E16:F16:G16');
+        $objPHPExcel->getActiveSheet()->setCellValue('A2', '');
+        $objPHPExcel->getActiveSheet()->setCellValue('A14', '');
+        $objPHPExcel->getActiveSheet()->setCellValue('B2', 'Count');
+        $objPHPExcel->setActiveSheetIndex(0)->mergeCells('B17:C17');
+        $objPHPExcel->setActiveSheetIndex(0)->mergeCells('D17:E17');
+        $objPHPExcel->setActiveSheetIndex(0)->mergeCells('F17:G17');
+        $objPHPExcel->getActiveSheet()->setCellValue('B16', 'Month');
+        $objPHPExcel->getActiveSheet()->setCellValue('B17', 'Air Time');
+        $objPHPExcel->getActiveSheet()->setCellValue('D17', 'Cash Sales');
+        $objPHPExcel->getActiveSheet()->setCellValue('F17', 'Bill Payment');
+        $objPHPExcel->getActiveSheet()->setCellValue('C2', 'Value');
+        $objPHPExcel->getActiveSheet()->setCellValue('A16', 'Region');
+		    $objPHPExcel->getActiveSheet()->getDefaultStyle()->applyFromArray($style);
+		    $objPHPExcel->getActiveSheet()->getStyle("$Header[$j]"."$i")->applyFromArray($styleArray);		  
+      }    
     }
  
 	function heading($heading,$objPHPExcel,$column) {
