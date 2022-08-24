0e84b6400af228054a6eb9d415b21679372649c0
icomfarook <farook.hussain@icomdatasystems.com>
2022-08-24 23:52:11 +0530

    Monthly Transaction Report issue FIxed

--- uirot/monthreportexcel.php
+++ uirot/monthreportexcel.php
@@ -5,7 +5,7 @@ require('../common/admin/configmysql.php');
 require('../common/sessioncheck.php');
 //include("excelfunctions.php");
 require_once   '../common/PHPExcel/Classes/PHPExcel/IOFactory.php';
-   $agentCode	= $_POST['agentCode'];
+   $Detail	= $_POST['Detail'];
    $MonthDate	= $_POST['MonthDate'];	
    $MonthDate = date("Y-m", strtotime($MonthDate,'-1 month'));
    $MonthName = date('M-Y', strtotime($MonthDate));
@@ -13,20 +13,37 @@ require_once   '../common/PHPExcel/Classes/PHPExcel/IOFactory.php';
        
 $title = "KadickMoni";
 
-$msg = "Monthly Report between $MonthDate ";
+$msg = "Monthly  Transaction Report For  $MonthName ";
 $objPHPExcel = new PHPExcel();
 
-           
-                $heading = array("Type",$MonthName);
-                $headcount = 3;
-                $query1 = "select 'Cashin' as type, format(ifNULL(sum(cashin_count),0),0) as count, format(ifNULL(sum(cashin_amount),0),2) as value from party_wallet_history where year(run_date) = year(current_date()) and month(run_date) = month(current_date()) and date_format(run_date,'%Y-%m') = '$MonthDate' UNION ALL select 'Cashout' as type, format(ifNULL(sum(cashout_count),0),0) as count, format(ifNULL(sum(cashout_amount),0),2) as value from party_wallet_history where year(run_date) = year(current_date()) and month(run_date) = month(current_date()) and date_format(run_date,'%Y-%m') = '$MonthDate' UNION ALL  select 'Billpayemnt' as type, format(ifNULL(sum(billpay_count),0),0) as count, format(ifNULL(sum(billpay_amount),0),2) as value from party_wallet_history where year(run_date) = year(current_date()) and month(run_date) = month(current_date()) and date_format(run_date,'%Y-%m') = '$MonthDate' UNION ALL select 'Airtime Recharge' as type, format(ifNULL(sum(evd_count),0),0) as count, format(ifNULL(sum(evd_amount),0),2) as value from party_wallet_history where year(run_date) = year(current_date()) and month(run_date) = month(current_date()) and date_format(run_date,'%Y-%m') = '$MonthDate' UNION ALL select 'Total' as type, format(sum(ifnull(cashin_count,0))+ sum(ifnull(cashout_count,0))+sum(ifnull(evd_count,0)) + sum(ifnull(billpay_count,0)),0) as count, format(sum(ifnull(cashin_amount,0)) + sum(ifnull(cashout_amount,0)) + sum(ifnull(billpay_amount,0)) + sum(ifnull(evd_count,0)),2) as value from party_wallet_history where year(run_date) = year(current_date()) and month(run_date) = month(current_date())  and date_format(run_date,'%Y-%m') = '$MonthDate'";
-                error_log("EXcelQuery".$query1);
+           if($Detail == true){
+            $heading = array("Type",$MonthName);
+            $headcount = 3;
+            $query1 = "select 'Cashin' as type, format(ifNULL(sum(cashin_count),0),0) as count, format(ifNULL(sum(cashin_amount),0),2) as value from party_wallet_history where year(run_date) = year(current_date()) and month(run_date) = month(current_date()) and date_format(run_date,'%Y-%m') = '$MonthDate' UNION ALL select 'Cashout' as type, format(ifNULL(sum(cashout_count),0),0) as count, format(ifNULL(sum(cashout_amount),0),2) as value from party_wallet_history where year(run_date) = year(current_date()) and month(run_date) = month(current_date()) and date_format(run_date,'%Y-%m') = '$MonthDate' UNION ALL  select 'Billpayemnt' as type, format(ifNULL(sum(billpay_count),0),0) as count, format(ifNULL(sum(billpay_amount),0),2) as value from party_wallet_history where year(run_date) = year(current_date()) and month(run_date) = month(current_date()) and date_format(run_date,'%Y-%m') = '$MonthDate' UNION ALL select 'Airtime Recharge' as type, format(ifNULL(sum(evd_count),0),0) as count, format(ifNULL(sum(evd_amount),0),2) as value from party_wallet_history where year(run_date) = year(current_date()) and month(run_date) = month(current_date()) and date_format(run_date,'%Y-%m') = '$MonthDate' UNION ALL select 'Total' as type, format(sum(ifnull(cashin_count,0))+ sum(ifnull(cashout_count,0))+sum(ifnull(evd_count,0)) + sum(ifnull(billpay_count,0)),0) as count, format(sum(ifnull(cashin_amount,0)) + sum(ifnull(cashout_amount,0)) + sum(ifnull(billpay_amount,0)) + sum(ifnull(evd_count,0)),2) as value from party_wallet_history where year(run_date) = year(current_date()) and month(run_date) = month(current_date())  and date_format(run_date,'%Y-%m') = '$MonthDate'";
+            error_log("EXcelQuery".$query1);
 
-               
-                 $query2 = "(select r.name as regions, format(sum(ifnull(h.evd_count,0)),0) as airtime_count, format(sum(ifnull(h.evd_amount,0)),2) as airtime_value from party_wallet_history h, agent_info a, state_list s, region_list r where r.region_id = s.region_id and a.state_id = s.state_id and a.agent_code = h.party_code and h.party_sales_type = 'External Agent' and r.active = 'Y'and year(h.run_date) = year(current_date()) and month(h.run_date) = month(current_date()) and date_format(h.run_date,'%Y-%m') = '$MonthDate' group by r.name order by r.name) UNION ALL (select r.name as regions, format(sum(ifnull(h.cashin_count,0)) + sum(ifnull(h.cashout_count,0)),0) as cashcount, format(sum(ifnull(h.evd_amount,0)) + sum(ifnull(cashout_amount,0)),2) as cash_value from party_wallet_history h, agent_info a, state_list s, region_list r where r.region_id = s.region_id and a.state_id = s.state_id and a.agent_code = h.party_code and h.party_sales_type = 'External Agent' and r.active = 'Y'and year(h.run_date) = year(current_date()) and month(h.run_date) = month(current_date()) and date_format(h.run_date,'%Y-%m') = '$MonthDate' group by r.name order by r.name) UNION ALL (select ANY_VALUE(r.name) as regions, format(sum(ifnull(h.billpay_count,0)),0) as billpay_count, format(sum(ifnull(h.billpay_amount,0)),2) as billpay_value from party_wallet_history h, agent_info a, state_list s, region_list r where r.region_id = s.region_id and a.state_id = s.state_id and a.agent_code = h.party_code and h.party_sales_type = 'External Agent' and r.active = 'Y'and year(h.run_date) = year(current_date()) and month(h.run_date) = month(current_date()) and date_format(h.run_date,'%Y-%m') = '$MonthDate' group by r.name order by r.name) UNION ALL (select 'Total' as regions, format(sum(ifnull(h.cashin_count,0)) + sum(ifnull(h.cashout_count,0)),0) as cashin_count, format(sum(ifnull(h.evd_amount,0)) + sum(ifnull(cashout_amount,0)),2) as cashin_value from party_wallet_history h, agent_info a, state_list s, region_list r where r.region_id = s.region_id and a.state_id = s.state_id and a.agent_code = h.party_code and h.party_sales_type = 'External Agent' and r.active = 'Y'and year(h.run_date) = year(current_date()) and month(h.run_date) = month(current_date()) and date_format(h.run_date,'%Y-%m') = '$MonthDate' group by r.name order by r.name)";
-                 error_log("detail query2 = ".$query2);
-                 $query3 = "select format(count(t.party_code) / (select count(*) from agent_info where party_sales_chain_id = 10)*100,2) as transact_percentage from (select party_code from party_wallet_history where year(run_date) = year(current_date()) and month(run_date) = month(current_date())   and date_format(run_date,'%Y-%m') = '$MonthAndYear' and (cashin_count > 0 or cashout_count > 0 or evd_count > 0 or billpay_count > 0) group by party_code) as t;";
-                 error_log("detail query3 = ".$query3);
+            $headcount2 = 7;
+            $query2 = "(select r.name as regions, format(sum(ifnull(h.evd_count,0)),0) as airtime_count , format(sum(ifnull(h.evd_amount,0)),2) as airtime_value, format(sum(ifnull(h.cashin_count,0)) + sum(ifnull(h.cashout_count,0)),0) as cash_count, format(sum(ifnull(h.evd_amount,0)) + sum(ifnull(cashout_amount,0)),2) as cash_value, format(sum(ifnull(h.billpay_count,0)),0) as billpay_count, format(sum(ifnull(h.billpay_amount,0)),2) as billpay_value from party_wallet_history h, agent_info a, state_list s, region_list r where r.region_id = s.region_id and a.state_id = s.state_id and a.agent_code = h.party_code  and r.active = 'Y'and year(h.run_date) = year(current_date()) and month(h.run_date) = month(current_date()) and date_format(h.run_date,'%Y-%m') =  '$MonthDate' group by r.name order by r.name)  UNION ALL (select 'Total' as regions, format(sum(ifnull(h.evd_count,0)),0) as airtime_count , format(sum(ifnull(h.evd_amount,0)),2) as airtime_value, format(sum(ifnull(h.cashin_count,0)) + sum(ifnull(h.cashout_count,0)),0) as cash_count, format(sum(ifnull(h.evd_amount,0)) + sum(ifnull(cashout_amount,0)),2) as cash_value, format(sum(ifnull(h.billpay_count,0)),0) as billpay_count, format(sum(ifnull(h.billpay_amount,0)),2) as billpay_value  from party_wallet_history h, agent_info a, state_list s, region_list r where r.region_id = s.region_id and a.state_id = s.state_id and a.agent_code = h.party_code  and r.active ='Y'and year(h.run_date) = year(current_date()) and month(h.run_date) = month(current_date()) and date_format(h.run_date,'%Y-%m') = '$MonthDate' group by regions)";
+             error_log("detail query2 = ".$query2);
+
+             $query3 = "select format(count(t.party_code) / (select count(*) from agent_info where party_sales_chain_id = 10)*100,2) as transact_percentage from (select party_code from party_wallet_history where year(run_date) = year(current_date()) and month(run_date) = month(current_date())   and date_format(run_date,'%Y-%m') = '$MonthDate' and (cashin_count > 0 or cashout_count > 0 or evd_count > 0 or billpay_count > 0) group by party_code) as t;";
+             error_log("detail query3 = ".$query3);
+           }
+           else{
+            $heading = array("Type",$MonthName);
+            $headcount = 3;
+            $query1 = "select 'Cashin' as type, format(ifNULL(sum(cashin_count),0),0) as count, format(ifNULL(sum(cashin_amount),0),2) as value from party_wallet_history where year(run_date) = year(current_date()) and month(run_date) = month(current_date()) and party_sales_type = 'External Agent' and date_format(run_date,'%Y-%m') = '$MonthDate' UNION ALL select 'Cashout' as type, format(ifNULL(sum(cashout_count),0),0) as count, format(ifNULL(sum(cashout_amount),0),2) as value from party_wallet_history where year(run_date) = year(current_date()) and month(run_date) = month(current_date()) and party_sales_type = 'External Agent' and date_format(run_date,'%Y-%m') = '$MonthDate' UNION ALL  select 'Billpayemnt' as type, format(ifNULL(sum(billpay_count),0),0) as count, format(ifNULL(sum(billpay_amount),0),2) as value from party_wallet_history where year(run_date) = year(current_date()) and month(run_date) = month(current_date()) and party_sales_type = 'External Agent' and date_format(run_date,'%Y-%m') = '$MonthDate' UNION ALL select 'Airtime Recharge' as type, format(ifNULL(sum(evd_count),0),0) as count, format(ifNULL(sum(evd_amount),0),2) as value from party_wallet_history where year(run_date) = year(current_date()) and month(run_date) = month(current_date()) and party_sales_type = 'External Agent' and date_format(run_date,'%Y-%m') = '$MonthDate' UNION ALL select 'Total' as type, format(sum(ifnull(cashin_count,0))+ sum(ifnull(cashout_count,0))+sum(ifnull(evd_count,0)) + sum(ifnull(billpay_count,0)),0) as count, format(sum(ifnull(cashin_amount,0)) + sum(ifnull(cashout_amount,0)) + sum(ifnull(billpay_amount,0)) + sum(ifnull(evd_count,0)),2) as value from party_wallet_history where year(run_date) = year(current_date()) and month(run_date) = month(current_date())  and party_sales_type = 'External Agent' and date_format(run_date,'%Y-%m') = '$MonthDate'";
+            error_log("EXcelQuery".$query1);
+
+            $headcount2 = 7;
+            $query2 = "(select r.name as regions, format(sum(ifnull(h.evd_count,0)),0) as airtime_count , format(sum(ifnull(h.evd_amount,0)),2) as airtime_value, format(sum(ifnull(h.cashin_count,0)) + sum(ifnull(h.cashout_count,0)),0) as cash_count, format(sum(ifnull(h.evd_amount,0)) + sum(ifnull(cashout_amount,0)),2) as cash_value, format(sum(ifnull(h.billpay_count,0)),0) as billpay_count, format(sum(ifnull(h.billpay_amount,0)),2) as billpay_value from party_wallet_history h, agent_info a, state_list s, region_list r where r.region_id = s.region_id and a.state_id = s.state_id and a.agent_code = h.party_code  and r.active = 'Y'and year(h.run_date) = year(current_date()) and month(h.run_date) = month(current_date()) and h.party_sales_type = 'External Agent' and date_format(h.run_date,'%Y-%m') =  '$MonthDate' group by r.name order by r.name)  UNION ALL (select 'Total' as regions, format(sum(ifnull(h.evd_count,0)),0) as airtime_count , format(sum(ifnull(h.evd_amount,0)),2) as airtime_value, format(sum(ifnull(h.cashin_count,0)) + sum(ifnull(h.cashout_count,0)),0) as cash_count, format(sum(ifnull(h.evd_amount,0)) + sum(ifnull(cashout_amount,0)),2) as cash_value, format(sum(ifnull(h.billpay_count,0)),0) as billpay_count, format(sum(ifnull(h.billpay_amount,0)),2) as billpay_value  from party_wallet_history h, agent_info a, state_list s, region_list r where r.region_id = s.region_id and a.state_id = s.state_id and a.agent_code = h.party_code  and r.active ='Y'and year(h.run_date) = year(current_date()) and month(h.run_date) = month(current_date())  and h.party_sales_type = 'External Agent' and date_format(h.run_date,'%Y-%m') = '$MonthDate' group by regions)";
+
+
+             error_log("detail query2 = ".$query2);
+             $query3 = "select format(count(t.party_code) / (select count(*) from agent_info where party_sales_chain_id = 10)*100,2) as transact_percentage from (select party_code from party_wallet_history where year(run_date) = year(current_date()) and month(run_date) = month(current_date()) and party_sales_type = 'External Agent'  and date_format(run_date,'%Y-%m') = '$MonthDate' and (cashin_count > 0 or cashout_count > 0 or evd_count > 0 or billpay_count > 0) group by party_code) as t;";
+             error_log("detail query3 = ".$query3);
+           }
+             
 
                  $result3 = mysqli_query($con,$query3);
                  $result2 = mysqli_query($con,$query2);
@@ -54,14 +71,26 @@ $objPHPExcel = new PHPExcel();
 		); 		
         
         for($j=0; $j<$column; $j++) {        
-            $objPHPExcel->getActiveSheet()->setCellValue("$Header[$j]"."$i","$row[$j]");
-            $objPHPExcel->getActiveSheet()->getColumnDimension($Header[$j])->setWidth(25);
-            $objPHPExcel->setActiveSheetIndex(0)->mergeCells('B1:C1');
+		  $objPHPExcel->getActiveSheet()->setCellValue("$Header[$j]"."$i","$row[$j]");
+          $objPHPExcel->getActiveSheet()->getColumnDimension($Header[$j])->setWidth(25);
+          $objPHPExcel->setActiveSheetIndex(0)->mergeCells('B1:C1');
+           // $objPHPExcel->setActiveSheetIndex(0)->mergeCells('B16:C16:D13:E16:F16:G16');
             $objPHPExcel->getActiveSheet()->setCellValue('A2', '');
             $objPHPExcel->getActiveSheet()->setCellValue('B2', 'Count');
             $objPHPExcel->getActiveSheet()->setCellValue('C2', 'Value');
-            $objPHPExcel->getActiveSheet()->getDefaultStyle()->applyFromArray($style);
-		    $objPHPExcel->getActiveSheet()->getStyle("$Header[$j]"."$i")->applyFromArray($styleArray);		  
+            //$objPHPExcel->getActiveSheet()->setCellValue('A14', '');
+            /*  $objPHPExcel->setActiveSheetIndex(0)->mergeCells('B17:C17');
+            $objPHPExcel->setActiveSheetIndex(0)->mergeCells('D17:E17');
+            $objPHPExcel->setActiveSheetIndex(0)->mergeCells('F17:G17');
+            $objPHPExcel->getActiveSheet()->setCellValue('B16', 'Month');
+            $objPHPExcel->getActiveSheet()->setCellValue('B17', 'Air Time');
+            $objPHPExcel->getActiveSheet()->setCellValue('D17', 'Cash Sales');
+            $objPHPExcel->getActiveSheet()->setCellValue('F17', 'Bill Payment');
+            $objPHPExcel->getActiveSheet()->setCellValue('C2', 'Value');
+            $objPHPExcel->getActiveSheet()->setCellValue('A16', 'Region'); */
+
+		  $objPHPExcel->getActiveSheet()->getDefaultStyle()->applyFromArray($style);
+		  $objPHPExcel->getActiveSheet()->getStyle("$Header[$j]"."$i")->applyFromArray($styleArray);		  
         }    
     }
     function generateExcel2 ($i,$row1,$objPHPExcel,$column) { 
@@ -85,8 +114,9 @@ $objPHPExcel = new PHPExcel();
             $MonthDate = date("Y-m", strtotime($MonthDate,'-1 month'));
             $MonthName = date('M-Y', strtotime($MonthDate));
            //error_log("MonthName ==".$MonthName);
-            $objPHPExcel->getActiveSheet()->setCellValue("$Header16[$j]"."$i","$row1[$j]");
-            $objPHPExcel->getActiveSheet()->getColumnDimension($Header16[$j])->setWidth(25);
+           $objPHPExcel->getActiveSheet()->setCellValue("$Header16[$j]"."$i","$row1[$j]");
+           $objPHPExcel->getActiveSheet()->getColumnDimension($Header16[$j])->setWidth(25);
+                    
             $objPHPExcel->getActiveSheet()->setCellValue('A16', 'Region'); 
             $objPHPExcel->setActiveSheetIndex(0)->mergeCells('B16:G16');
             $objPHPExcel->getActiveSheet()->getDefaultStyle()->applyFromArray($style);
@@ -99,7 +129,6 @@ $objPHPExcel = new PHPExcel();
             $objPHPExcel->getActiveSheet()->setCellValue('D17', 'Cash Sales');
             $objPHPExcel->getActiveSheet()->setCellValue('F17', 'Bill Payment');
             $objPHPExcel->getActiveSheet()->setCellValue('C2', 'Value');
-            $objPHPExcel->getActiveSheet()->setCellValue('A16', 'Region');
             $objPHPExcel->getActiveSheet()->setCellValue('B18', 'Count');
             $objPHPExcel->getActiveSheet()->setCellValue('C18', 'Value');
             $objPHPExcel->getActiveSheet()->setCellValue('D18', 'Count');
@@ -108,12 +137,12 @@ $objPHPExcel = new PHPExcel();
             $objPHPExcel->getActiveSheet()->setCellValue('G18', 'Value');
             $objPHPExcel->getActiveSheet()->getStyle("A2:C2")->applyFromArray($styleArray);
             $objPHPExcel->getActiveSheet()->getStyle("A18:G18")->applyFromArray($styleArray);
-            $objPHPExcel->getActiveSheet()->getStyle("A16")->applyFromArray($styleArray);
-            $objPHPExcel->getActiveSheet()->getStyle("B16:G16")->applyFromArray($styleArray);
-            $objPHPExcel->getActiveSheet()->getStyle("B17:C17")->applyFromArray($styleArray);
-            $objPHPExcel->getActiveSheet()->getStyle("D17:E17")->applyFromArray($styleArray);
-            $objPHPExcel->getActiveSheet()->getStyle("F17:G17")->applyFromArray($styleArray);
-            $objPHPExcel->getActiveSheet()->getStyle("$Header16[$j]"."$i")->applyFromArray($styleArray);		  
+           $objPHPExcel->getActiveSheet()->getStyle("A16")->applyFromArray($styleArray);
+           $objPHPExcel->getActiveSheet()->getStyle("B16:G16")->applyFromArray($styleArray);
+           $objPHPExcel->getActiveSheet()->getStyle("B17:C17")->applyFromArray($styleArray);
+           $objPHPExcel->getActiveSheet()->getStyle("D17:E17")->applyFromArray($styleArray);
+           $objPHPExcel->getActiveSheet()->getStyle("F17:G17")->applyFromArray($styleArray);
+           $objPHPExcel->getActiveSheet()->getStyle("$Header16[$j]"."$i")->applyFromArray($styleArray);		  
 
         }
         
@@ -217,7 +246,8 @@ $objPHPExcel = new PHPExcel();
        }
         				
        while ($row = mysqli_fetch_array($result2))	{
-              generateExcel2 ($secondTable, $row,$objPHPExcel,$headcount);
+      
+              generateExcel2 ($secondTable, $row,$objPHPExcel,$headcount2);
         $secondTable++;
                     
     }
